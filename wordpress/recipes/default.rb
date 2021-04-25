beffaf81d9d44896fd476c370963ae6b7447f936
Yutaro Mori <yuta_mori@outlook.jp>
2021-04-26 01:27:08 +0900

    add wordpress

---
+++ wordpress/recipes/default.rb
@@ -0,0 +1,17 @@
+remote_file "/tmp" do
+    source "https://ja.wordpress.org/latest-ja.tar.gz"
+    owner "root"
+    group "root"
+    mode "0644"
+end
+
+script "install_wordpress" do
+    interpreter "bash"
+    user "root"
+    cwd "/tmp"
+    code <<-EOH
+      tar latest-ja.tar.gz
+      mv wordpress /var/www/html/
+      chown -R www-data:www-data /var/www/html/wordpress
+    EOH
+end
